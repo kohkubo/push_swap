5716c68a0f713d79734f0de2a301867ecf6b365a
kohkubo <kohkubo@student.42tokyo.jp>
2021-07-07 15:50:08 +0900

    fix(libbclst): add null check in bclstsize.c

--- lib/libbclst/bclstsize.c
+++ lib/libbclst/bclstsize.c
@@ -5,13 +5,14 @@ int	bclstsize(t_bclst *lst)
 	int	len;
 	t_bclst	*tmp;
 
+	if (lst == NULL)
+		return (0);
 	tmp = bclstsentry(lst);
 	tmp = tmp->next;
 	len = 0;
 	while (tmp->content != NULL)
 	{
 		tmp = tmp->next;
-		printf("%p\n", tmp);
 		len++;
 	}
 	return (len);
