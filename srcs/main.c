b75d9ed1d1ed663877aa06dd2da2c7a1d45e5b7f
kohkubo <kohkubo@student.42tokyo.jp>
2021-07-11 14:50:37 +0900

    add : resolve_only_push()

--- srcs/main.c
+++ srcs/main.c
@@ -18,9 +18,38 @@ void	divide(t_bclst **lst_a, t_bclst **lst_b, int pivot)
 	*lst_a = bclstfirst(*lst_a);
 }
 
+void	resolve_only_push(t_bclst **lst_a, t_bclst **lst_b)
+{
+	t_content	*c;
+	int	max;
+	int	i;
+
+	i = 0;
+	max = bclstsize(*lst_a);
+	while (i < max)
+	{
+		c = (t_content *)((*lst_a)->content);
+		if (c->index == i)
+		{
+			ft_pa(lst_a, lst_b);
+			i++;
+			continue ;
+		}
+		if ((*lst_a)->next->content == NULL)
+			*lst_a = (*lst_a)->next->next;
+		else
+			*lst_a = (*lst_a)->next;
+	}
+	while ((*lst_b)->content != NULL)
+	{
+		ft_pb(lst_a, lst_b);
+	}
+}
+
 void	ps_do(t_bclst **lst_a, t_bclst **lst_b)
 {
-	divide(lst_a, lst_b, bclstsize(*lst_a) / 2);
+	// divide(lst_a, lst_b, bclstsize(*lst_a) / 2);
+	// resolve_only_push(lst_a, lst_b);
 
 	printf("=================\n");
 	ps_print(*lst_a, *lst_b);
