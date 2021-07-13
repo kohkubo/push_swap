d623e57428e2b6b3e1925c8c53fad1b7460dd3c6
kohkubo <kohkubo@student.42tokyo.jp>
2021-07-13 23:58:42 +0900

    refactor : alltest.sh

--- alltest.sh
+++ alltest.sh
@@ -3,15 +3,33 @@ gen_permutation() {
   echo -e "from itertools import permutations\nfor i in permutations(list(range(0, $1))):\n  print(*i)" | python3
 }
 
+test_all() {
+  while read line; do
+    count=$(./push_swap ${line} | wc -l | tr -d ' ')
+    checker_result=$(./push_swap ${line} | ./checker ${line})
+    if [ ${checker_result} == "KO" ]; then
+      echo "${line}" >> error_case.txt
+    fi
+    # printf "${line}: count: ${count} checker: ${checker_result}\n"
+  done < permutation.txt
+}
+
 rm -rf error_case.txt res
 
-gen_permutation $1 > permutation.txt
+gen_permutation 1 > permutation.txt
+test_all
 
-while read line; do
-  count=$(./push_swap ${line} | wc -l | tr -d ' ')
-  checker_result=$(./push_swap ${line} | ./checker ${line})
-  if [ ${checker_result} == "KO" ]; then
-    echo "${line}" >> error_case.txt
-  fi
-  printf "${line}: count: ${count} checker: ${checker_result}\n"
-done < permutation.txt
+gen_permutation 2 > permutation.txt
+test_all
+
+gen_permutation 3 > permutation.txt
+test_all
+
+gen_permutation 4 > permutation.txt
+test_all
+
+gen_permutation 5 > permutation.txt
+test_all
+
+gen_permutation 6 > permutation.txt
+test_all
