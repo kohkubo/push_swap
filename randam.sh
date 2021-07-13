64c5d811bcb002ea231829079f8e8317d3dcc3f9
kohkubo <kohkubo@student.42tokyo.jp>
2021-07-14 00:26:01 +0900

    add : randam.sh

---
+++ randam.sh
@@ -0,0 +1,23 @@
+# make re
+gen_permutation() {
+  echo -e "import random\nprint(*random.sample(list(range(0, 10)), $1))" | python3
+}
+
+test_all() {
+  while read line; do
+    count=$(./push_swap ${line} | wc -l | tr -d ' ')
+    checker_result=$(./push_swap ${line} | ./checker ${line})
+    if [ ${checker_result} == "KO" ]; then
+      echo "${line}" >> error_case.txt
+    fi
+    printf "${line}: count: ${count} checker: ${checker_result}\n"
+  done < permutation.txt
+}
+
+rm -rf error_case.txt res permutation.txt
+
+for ((i=0; i < 10; i++)); do
+  gen_permutation $1 >> permutation.txt
+done
+
+test_all
