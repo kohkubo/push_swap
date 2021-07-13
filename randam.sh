1a0d0369881f14676c383251831e946eb7dfd21a
kohkubo <kohkubo@student.42tokyo.jp>
2021-07-13 18:46:55 +0900

    add : randam.sh

---
+++ randam.sh
@@ -0,0 +1,12 @@
+make re
+gen_permutation() {
+  echo -e "from itertools import permutations\nfor i in permutations(list(range(0, $1))):\n  print(*i)" | python3
+}
+
+gen_permutation 3 > permutation.txt
+
+while read line; do
+  count=$(./push_swap ${line} | wc -l | tr -d ' ')
+  checker_result=$(./push_swap ${line} | ./checker ${line})
+  printf "${line}: count: ${count} checker: ${checker_result}\n"
+done < permutation.txt
